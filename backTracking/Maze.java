69f1b65c1e0425904f27d244e99f09a68fd3ae5b
srisathvik <komminenisathvik@gmail.com>
2024-08-19 15:23:11 +0530

    backtracking

---
+++ backTracking/Maze.java
@@ -0,0 +1,48 @@
+package backTracking;
+
+import java.util.*;;
+
+public class Maze {
+    public static void main(String[] args) {
+        // path("", 3, 3);
+        System.out.println(pathRet("", 3, 3));
+    }
+
+    static int count(int r, int c) {
+        if (r == 1 || c == 1) {
+            return 1;
+        }
+        int up = count(r - 1, c);
+        int left = count(r, c - 1);
+        return up + left;
+    }
+
+    static void path(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            System.out.println(p);
+            return;
+        }
+        if (r > 1) {
+            path(p + "D", r - 1, c);
+        }
+        if (c > 1) {
+            path(p + "R", r, c - 1);
+        }
+    }
+
+    static ArrayList<String> pathRet(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if (r > 1) {
+            list.addAll(pathRet(p + "D", r - 1, c));
+        }
+        if (c > 1) {
+            list.addAll(pathRet(p + "R", r, c - 1));
+        }
+        return list;
+    }
+}
