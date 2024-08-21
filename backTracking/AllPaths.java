706a627d45d7e74168873b8a9cc457c15f9bb288
srisathvik <komminenisathvik@gmail.com>
2024-08-21 12:25:01 +0530

    maze problem

--- backTracking/AllPaths.java
+++ backTracking/AllPaths.java
@@ -1,26 +1,72 @@
-// TODO
+import java.util.Arrays;
 
-// package backTracking;
+public class AllPaths {
+    public static void main(String[] args) {
+        boolean[][] maze = {
+                { true, true, true },
+                { true, true, true },
+                { true, true, true },
+        };
+        // allPaths("", maze, 0, 0);
+        int[][] board = new int[maze.length][maze[0].length];
+        allPathPrint("", maze, 0, 0, board, 1);
 
-// import java.util.ArrayList;
+    }
 
-// public class AllPaths {
-// public static void main(String[] args) {
+    static void allPaths(String p, boolean[][] maze, int r, int c) {
 
-// }
-// static void pathwithRestriction(String p, boolean[][] maze, int r, int c) {
-// if (r == maze.length - 1 && c == maze[0].length - 1) {
-// System.out.println(p);
-// return;
-// }
-// if (!maze[r][c]) {
-// return;
-// }
-// if (r < maze.length - 1) {
-// pathwithRestriction(p + "D", maze, r + 1, c);
-// }
-// if (c < maze[0].length - 1) {
-// pathwithRestriction(p + "R", maze, r, c + 1);
-// }
-// }
-// }
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            System.out.println(p);
+            return;
+        }
+        if (!maze[r][c]) {
+            return;
+        }
+        maze[r][c] = false;
+        if (r < maze.length - 1) {
+            allPaths(p + "D", maze, r + 1, c);
+        }
+        if (c < maze[0].length - 1) {
+            allPaths(p + "R", maze, r, c + 1);
+        }
+        if (r > 0) {
+            allPaths(p + "U", maze, r - 1, c);
+        }
+        if (c > 0) {
+            allPaths(p + "L", maze, r, c - 1);
+        }
+        maze[r][c] = true;
+    }
+
+    static void allPathPrint(String p, boolean[][] maze, int r, int c, int[][] path, int step) {
+
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            path[r][c] = step;
+            for (int[] arr : path) {
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+        if (!maze[r][c]) {
+            return;
+        }
+        maze[r][c] = false;
+        path[r][c] = step;
+        if (r < maze.length - 1) {
+            allPathPrint(p + "D", maze, r + 1, c, path, step + 1);
+        }
+        if (c < maze[0].length - 1) {
+            allPathPrint(p + "R", maze, r, c + 1, path, step + 1);
+        }
+        if (r > 0) {
+            allPathPrint(p + "U", maze, r - 1, c, path, step + 1);
+        }
+        if (c > 0) {
+            allPathPrint(p + "L", maze, r, c - 1, path, step + 1);
+        }
+        maze[r][c] = true;
+        path[r][c] = 0;
+    }
+}
